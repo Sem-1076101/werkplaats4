7ebb153f83dd39b1c4d747876a2a6ec08a700298
Bryan-1065913 <1065913@hr.nl>
2024-04-26 17:00:11 +0200

    Changed routes

--- client/App.js
+++ client/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Index from './components';
+import Index from './components/Index';
+import Dashboard from './components/Dashboard';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Index />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
