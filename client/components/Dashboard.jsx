08509d34daf1bd5af392b21b977a57cd1d92799a
Bryan-1065913 <1065913@hr.nl>
2024-04-26 22:23:11 +0200

    Tried to add modal for domains

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import BaseLayout from './BaseLayout';
 
 function Dashboard() {
@@ -16,7 +16,33 @@ function Dashboard() {
                     </div>
                     <div className="col-md-6">
                         <h2>Domeinen</h2>
-                        <p>Je hebt nog geen cursussen gevolgd. Klik <a href="">hier</a> om een domein te volgen.</p>
+                        <p>Je hebt nog geen domein toegevoegd. Klik <a href="#" data-bs-toggle="modal"
+                                                                       data-bs-target="#myModal">hier</a> om een domein
+                            te volgen.</p>
+                        <button type="button" className="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                            Launch demo modal
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                 aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
                 </div>
             </div>
