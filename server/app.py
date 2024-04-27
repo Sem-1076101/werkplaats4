cab76844264a51948479eeee4467768f2c82b5ae
Bryan-1065913 <1065913@hr.nl>
2024-04-27 13:46:10 +0200

    Updated app with new url

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 import datetime
 from flask_cors import CORS
+from database import get_all_categories_from_database
 
 app = Flask(__name__)
 CORS(app)
@@ -11,9 +12,9 @@ def inject_current_year():
     return {'current_year': datetime.datetime.now().year}
 
 
-@app.route('/api')
-def api():
-    data = {'members': ['member1', 'member2', 'member3']}
+@app.route('/api/dashboard', methods=['GET'])
+def get_data():
+    data = get_all_categories_from_database()
     return jsonify(data)
 
 
