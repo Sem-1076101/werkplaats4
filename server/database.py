91bfed07d5a0b67bc9878502536f6b206227be8c
Bryan-1065913 <1065913@hr.nl>
2024-04-29 21:02:03 +0200

    Set new database query

--- server/database.py
+++ server/database.py
@@ -15,7 +15,6 @@ def get_all_categories_from_database():
     data = []
     for row in categories:
         row_dict = dict(zip(columns, row))
-        # Convert the BLOB image data to base64
         if row_dict['course_image']:
             row_dict['course_image'] = base64.b64encode(row_dict['course_image']).decode('utf-8')
         data.append(row_dict)
@@ -33,6 +32,14 @@ def get_student_domain(studentnumber):
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute("SELECT course_id FROM students WHERE studentnumber=?", (studentnumber,))
+    result = cursor.fetchone()
+    conn.close()
+    return result[0] if result else None
+
+def get_course_name(course_id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT course_name FROM domains WHERE course_id=?", (course_id,))
     result = cursor.fetchone()
     conn.close()
     return result[0] if result else None
