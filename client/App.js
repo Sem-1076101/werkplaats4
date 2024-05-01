a33877c223e0769010c6a145402e3edb08b68728
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-01 13:04:18 +0200

    Module pagina aangemaakt + route

--- client/App.js
+++ client/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Index from './components/Index';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Index from "./components/Index";
+import Dashboard from "./components/Dashboard";
+import Modules from "./components/Modules";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/modules" element={<Modules />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
