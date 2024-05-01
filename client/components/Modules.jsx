2a178b6fa5701925b81e4b081a7c98c49ed91750
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-01 13:04:18 +0200

    Module pagina aangemaakt + route

---
+++ client/components/Modules.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import BaseLayout from "./BaseLayout";
+
+function Modules() {
+  return (
+    <BaseLayout>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Beschikbare modules</h1>
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
+}
+
+export default Modules;
