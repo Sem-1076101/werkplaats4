79257758497e1e3bbd5d55de282f013c28cd9fdd
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-02 15:06:45 +0200

    gegevens uit database ophalen

--- client/components/Modules.jsx
+++ client/components/Modules.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import BaseLayout from "./BaseLayout";
+import connection from "../api";
+import { get_modules } from "../api";
 
 function Modules() {
+  const [modules, setModules] = useState([]);
+
+  useEffect(() => {
+    getModulesData();
+  }, []);
+
+  const getModulesData = () => {
+    get_modules()
+      .then(data => {
+        setModules(data);
+      })
+      .catch(error => {
+        console.error("Error fetching modules:", error);
+      })
+  };
+
+
   return (
     <BaseLayout>
       <div className="container">
         <div className="row">
           <div className="col-12">
             <h1>Beschikbare modules</h1>
+            <ul>
+              {modules.map(module => (
+                <li key={module.id}>
+                  <p>{module.module_name}</p>
+                  <p>{module.description}</p>
+                  <p>{module.progress_indicator}</p>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
