1095d12c6cc47d0537680be99ce0fcda1f3f28aa
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-02 16:16:19 +0200

    Je ziet nu modules, nog niet mooi en hoe het hoort.

--- client/components/Modules.jsx
+++ client/components/Modules.jsx
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import BaseLayout from "./BaseLayout";
-import connection from "../api";
 import { get_modules } from "../api";
 
 function Modules() {
-  const [modules, setModules] = useState([]);
+  const [modules, setModules] = useState(null);
 
   useEffect(() => {
     getModulesData();
@@ -12,14 +11,14 @@ function Modules() {
 
   const getModulesData = () => {
     get_modules()
-      .then(data => {
-        setModules(data);
+      .then(modules => {
+        console.log(modules)
+        setModules(modules);
       })
       .catch(error => {
         console.error("Error fetching modules:", error);
-      })
+      });
   };
-
 
   return (
     <BaseLayout>
@@ -27,15 +26,19 @@ function Modules() {
         <div className="row">
           <div className="col-12">
             <h1>Beschikbare modules</h1>
-            <ul>
-              {modules.map(module => (
-                <li key={module.id}>
-                  <p>{module.module_name}</p>
-                  <p>{module.description}</p>
-                  <p>{module.progress_indicator}</p>
-                </li>
-              ))}
-            </ul>
+            <div>
+              {modules ? (
+                modules.map((module, index) => (
+                  <div key={index}>
+                    <p>{module[1]}</p>
+                    <p>{module[2]}</p>
+                    <p>{module[3]}</p>
+                  </div>
+                ))
+              ) : (
+                <p>Modules nog niet beschikbaar</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
