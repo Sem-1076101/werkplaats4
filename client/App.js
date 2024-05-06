f7969f80083e0219f8b8bc82cfab90038ef3037d
Bryan-1065913 <1065913@hr.nl>
2024-05-06 12:34:05 +0200

    Changes for app urls

--- client/App.js
+++ client/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './components/Index';
 import Dashboard from './components/Dashboard';
+import Platform from './components/Platform';
+import EditDomain from './components/EditDomain';
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/platform" element={<Platform />} />
+                <Route path="/wijzig/domein/:id" element={<EditDomain />} />
             </Routes>
         </Router>
     );
