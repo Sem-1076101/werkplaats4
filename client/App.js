4ca5b51039df43be8e1b2d9836f0078ac702ab69
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-08 11:21:42 +0200

    app.js routes

--- client/App.js
+++ client/App.js
@@ -1,19 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Index from "./components/Index";
-import Dashboard from "./components/Dashboard";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Index from './components/Index';
+import Dashboard from './components/Dashboard';
+import Platform from './components/Platform';
+import EditDomain from './components/EditDomain';
+import AddDomain from './components/AddDomain';
 import Modules from "./components/Modules";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/modules" element={<Modules />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/platform" element={<Platform />} />
+                <Route path="/wijzig/domein/:id" element={<EditDomain />} />
+                <Route path="/aanmaken/domein/" element={<AddDomain />} />
+                <Route path="/modules" element={<Modules />} />
+            
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
+
+
+
+
+
+
+
