94c1621a524a1a841696a0b2e82dba058ed06050
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-09 14:21:17 +0200

    Modules query gemaakt en gegevens opgehaald nog niet helemaal af

--- client/components/Modules.jsx
+++ client/components/Modules.jsx
@@ -1,49 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import BaseLayout from "./BaseLayout";
-import { get_modules } from "../api";
+import {get_modules} from "../api";
+import { useParams } from "react-router-dom";
+
+// In je component
 
 function Modules() {
-  const [modules, setModules] = useState(null);
+    const { id } = useParams();
+    const [modules, setModules] = useState(null);
 
-  useEffect(() => {
-    getModulesData();
-  }, []);
+    const [showMedal, setShowModal] = useState(false);
+    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 
-  const getModulesData = () => {
-    get_modules()
-      .then(modules => {
-        console.log(modules)
-        setModules(modules);
-      })
-      .catch(error => {
-        console.error("Error fetching modules:", error);
-      });
-  };
+    const handleOpenModal = () => {
+        setShowModal(true);
+    }
 
-  return (
-    <BaseLayout>
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <h1>Beschikbare modules</h1>
-            <div>
-              {modules ? (
-                modules.map((module, index) => (
-                  <div key={index}>
-                    <p>{module[1]}</p>
-                    <p>{module[2]}</p>
-                    <p>{module[3]}</p>
-                  </div>
-                ))
-              ) : (
-                <p>Modules nog niet beschikbaar</p>
-              )}
+    const handleCloseModal = () => {
+        setShowModal(false);
+    }
+
+
+    useEffect(() => {
+        getModulesData();
+    }, []);
+
+    const getModulesData = () => {
+        const url =`http://localhost:19006/api/modules/${id}`;
+        console.log('Fetching data from:', url);
+
+        get_modules(id)
+            .then(modules => {
+                console.log(modules)
+                setModules(modules);
+            })
+            .catch(error => {
+                console.error("Error fetching modules:", error);
+                console.error("ERror details:", error.response.data);
+            });
+    };
+
+
+    return (
+        <BaseLayout>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Beschikbare modules</h1>
+                        <div>
+                            {modules ? (
+                                modules.map((module, index) => (
+                                    <div key={index}>
+                                        <h2>{module.module_name}</h2>
+                                        <p>{module.description}</p>
+                                    </div>
+                                ))
+                            ) : (
+                                <div className="mt-5">
+                                    <h2>Modules nog niet beschikbaar bij dit domein.</h2>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </BaseLayout>
-  );
+        </BaseLayout>
+    );
 }
 
 export default Modules;
