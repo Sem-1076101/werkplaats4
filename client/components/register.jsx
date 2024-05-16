aa9ef2dddc090f9a03b3bc660e1ff40ec0af5205
hamza-1085504 <1085504@hr.nl>
2024-05-16 05:50:30 +0200

    bootstrap gebruikt voor de login en registratie

--- client/components/register.jsx
+++ client/components/register.jsx
@@ -22,14 +22,29 @@ function Register() {
     };
 
     return (
-        <div>
+        <div className="container">
             <h2>Registratie</h2>
-            <input type="email" name="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
-            <input type="password" name="password" placeholder="Wachtwoord" value={formData.password} onChange={handleChange} />
-            <input type="text" name="first_name" placeholder="Voornaam" value={formData.first_name} onChange={handleChange} />
-            <input type="text" name="last_name" placeholder="Achternaam" value={formData.last_name} onChange={handleChange} />
-            <input type="text" name="studentnumber" placeholder="Studentnummer" value={formData.studentnumber} onChange={handleChange} />
-            <button onClick={handleRegister}>Registreren</button>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">E-mail</label>
+                <input type="email" className="form-control" id="email" name="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Wachtwoord</label>
+                <input type="password" className="form-control" id="password" name="password" placeholder="Wachtwoord" value={formData.password} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="first_name" className="form-label">Voornaam</label>
+                <input type="text" className="form-control" id="first_name" name="first_name" placeholder="Voornaam" value={formData.first_name} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="last_name" className="form-label">Achternaam</label>
+                <input type="text" className="form-control" id="last_name" name="last_name" placeholder="Achternaam" value={formData.last_name} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="studentnumber" className="form-label">Studentnummer</label>
+                <input type="text" className="form-control" id="studentnumber" name="studentnumber" placeholder="Studentnummer" value={formData.studentnumber} onChange={handleChange} />
+            </div>
+            <button className="btn btn-primary" onClick={handleRegister}>Registreren</button>
         </div>
     );
 }
