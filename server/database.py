33222ad59331c0bcf5a7e1f5843e5a7a84ca91a7
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-21 13:03:37 +0200

    query level aangemaakt

--- server/database.py
+++ server/database.py
@@ -107,3 +107,17 @@ def get_modules_from_database_by_domain_id(domain_id):
         row_dict = dict(zip(columns, row))
         data.append(row_dict)
     return data
+
+
+def get_level_by_module_id(module_id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM levels WHERE module_id=?", (module_id,))
+    columns = [column[0] for column in cursor.description]
+    levels = cursor.fetchall()
+    conn.close()
+    data = []
+    for row in levels:
+        row_dict = dict(zip(columns, row))
+        data.append(row_dict)
+    return data
