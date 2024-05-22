20fb0448c5b9d3f23d02dd8b6854187bd762bc56
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-22 12:35:38 +0200

    Routes aangemaakt voor platform modules

--- client/App.js
+++ client/App.js
@@ -7,6 +7,7 @@ import EditDomain from './components/EditDomain';
 import AddDomain from './components/AddDomain';
 import Modules from "./components/Modules";
 import Levels from "./components/Levels";
+import Platform_modules from "./components/Platform_modules";
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
                 <Route path="/aanmaken/domein/" element={<AddDomain />} />
                 <Route path="/modules/:domain_id" element={<Modules />} />
                 <Route path="/levels/:module_id" element={<Levels />} />
+                <Route path="/platform/modules" element={<Platform_modules />} />
 
             </Routes>
         </Router>
