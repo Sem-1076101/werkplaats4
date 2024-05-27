827390596ce6a440a9def8649c5519c8f744e7f5
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-05-27 17:35:31 +0200

    Begin gemaakt aan toevoegen modules, nog even kijken naar hoe ik alle domeinen toon in een select option zodat het domein_id klopt

--- client/components/AddModule.jsx
+++ client/components/AddModule.jsx
@@ -1,14 +1,28 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import BaseLayout from './BaseLayout';
-import {addModule} from '../api'
+import {getModules, addModule} from '../api'
 import {Link} from "react-router-dom";
 
 function AddModule() {
     const [module, setModule] = useState({
         module_name: '',
         description: '',
-        progress_indicator: ''
+        progress_indicator: '',
+        domain_id: ''
     });
+
+
+    const [modules, setPlatform_modules] = useState(null);
+
+    useEffect(() => {
+        getModules()
+            .then(responseData => {
+                setPlatform_modules(responseData);
+            })
+            .catch(error => {
+                console.error('Er was een fout bij het ophalen van de data:', error);
+            });
+    }, []);
 
     const handleChange = (event) => {
         setModule({
@@ -17,28 +31,16 @@ function AddModule() {
         });
     };
 
-    // const handleImageChange = (event) => {
-    //     const file = event.target.files[0];
-    //     const reader = new FileReader();
-    //     reader.onloadend = () => {
-    //         const base64String = reader.result.replace('data:image/jpeg;base64,', '');
-    //         setDomain({
-    //             ...domain,
-    //             course_image: base64String
-    //         });
-    //     };
-    //     reader.readAsDataURL(file);
-    // };
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        addDomain(domain)
+        addModule(module)
             .then(() => {
-                alert('Domein succesvol toegevoegd!');
-                setDomain({
-                    course_name: '',
-                    course_description: '',
-                    course_image: ''
+                alert('Module succesvol toegevoegd!');
+                setModule({
+                    module_name: '',
+                    description: '',
+                    progress_indicator: '',
+                    domain_id: ''
                 });
             })
             .catch(error => {
@@ -50,24 +52,30 @@ function AddModule() {
         <BaseLayout>
             <div className="container">
                 <div className="d-flex justify-content-between align-items-center">
-                    <h1>Voeg een nieuw domein toe</h1>
-                    <Link to="/platform" className="btn btn-secondary">Terug naar Platform</Link>
+                    <h1>Voeg een nieuwe module toe</h1>
+                    <Link to="/platform/modules" className="btn btn-secondary">Terug naar module-platform</Link>
                 </div>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
-                        <label htmlFor="course_name" className="form-label">Domein naam</label>
+                        <label htmlFor="course_name" className="form-label">Module naam</label>
                         <input type="text" className="form-control" id="course_name" name="course_name"
-                               value={domain.course_name} onChange={handleChange} required/>
+                               value={module.module_name} onChange={handleChange} required/>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="course_description" className="form-label">Domein beschrijving</label>
-                        <textarea className="form-control" id="course_description" name="course_description"
-                                  value={domain.course_description} onChange={handleChange} required/>
+                        <label htmlFor="description" className="form-label">Domein beschrijving</label>
+                        <textarea className="form-control" id="description" name="description"
+                                  value={module.description} onChange={handleChange} required/>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="course_image" className="form-label">Domein afbeelding</label>
-                        <input type="file" className="form-control" id="course_image" name="course_image" accept="image/png, image/jpeg"
-                               onChange={handleImageChange} required/>
+                        <label htmlFor="progression_indicator" className="form-label">Progressie indicator</label>
+                        <input type="text" className="form-control" id="progression_indicator" name="progression_indicator"
+                               value={module.module_name} onChange={handleChange} required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="domain_name" className="form-label">Domein:</label>
+                        <select id="domain_name" name="domain_name" onChange={handleChange} required>
+                            <option value={module.domain_id}>{module.domain_name}</option>
+                        </select>
                     </div>
                     <button type="submit" className="btn btn-primary">Voeg toe</button>
                 </form>
