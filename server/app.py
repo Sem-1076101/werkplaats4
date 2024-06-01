ecf36f9ca8bfd09377c5be16aef568fb9b47d8b6
Bryan-1065913 <1065913@hr.nl>
2024-06-01 15:10:38 +0200

    Edited ip for flask server

--- server/app.py
+++ server/app.py
@@ -146,5 +146,5 @@ def create_domain():
         return result
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='192.168.1.142', port=5000)
