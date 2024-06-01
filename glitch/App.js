9daf09f5d91859509f961491387895b4135aaf2b
Bryan-1065913 <1065913@hr.nl>
2024-06-01 15:13:03 +0200

    Edited react app with new urls

--- glitch/App.js
+++ glitch/App.js
@@ -1,23 +1,44 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {ThemeProvider} from './components/ThemeContext';
+import React, { useEffect, useState } from 'react';
+import isWeb from './isWeb';
+import { createStackNavigator } from '@react-navigation/stack';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { ThemeProvider } from './components/ThemeContext';
 import Index from './components/Index';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import BaseLayout from './components/BaseLayout';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
-const Routes = () => {
-    return (
-        <ThemeProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home">
-                    <Stack.Screen name="Home" component={Index}/>
-                    {/* Voeg hier andere schermen toe, zoals Login en Register */}
-                </Stack.Navigator>
-            </NavigationContainer>
-        </ThemeProvider>
-    );
-};
+function App() {
+    if (isWeb) {
+        return (
+            <ThemeProvider>
+                <Router>
+                    <BaseLayout>
+                        <Routes>
+                            <Route path="/" element={<Index />} />
+                            <Route path="/home" element={<Index />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Routes>
+                    </BaseLayout>
+                </Router>
+            </ThemeProvider>
+        );
+    } else {
+        return (
+            <ThemeProvider>
+                <BaseLayout>
+                    <Stack.Navigator initialRouteName="Home">
+                        <Stack.Screen name="Home" component={Index} />
+                        <Stack.Screen name="Login" component={Login} />
+                        <Stack.Screen name="Dashboard" component={Dashboard} />
+                    </Stack.Navigator>
+                </BaseLayout>
+            </ThemeProvider>
+        );
+    }
+}
 
-export default Routes;
-
+export default App;
