db1099af62ad64cc082796dfc15a9141e723a9d7
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-06-03 21:31:37 +0200

    Module terug naar react nromaal

--- client/components/AddModule.jsx
+++ client/components/AddModule.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet, Picker } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { getModules, addModule } from '../api'; // Zorg ervoor dat je de juiste pad hebt
+import React, {useEffect, useState} from 'react';
+import BaseLayout from './BaseLayout';
+import {getModules, addModule} from '../api'
+import {Link} from "react-router-dom";
 
 function AddModule() {
     const [module, setModule] = useState({
@@ -11,30 +11,31 @@ function AddModule() {
         domain_id: ''
     });
 
-    const [modules, setPlatformModules] = useState([]);
-    const navigation = useNavigation();
+
+    const [modules, setPlatform_modules] = useState(null);
 
     useEffect(() => {
         getModules()
             .then(responseData => {
-                setPlatformModules(responseData);
+                setPlatform_modules(responseData);
             })
             .catch(error => {
                 console.error('Er was een fout bij het ophalen van de data:', error);
             });
     }, []);
 
-    const handleChange = (name, value) => {
+    const handleChange = (event) => {
         setModule({
             ...module,
-            [name]: value
+            [event.target.name]: event.target.value
         });
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
         addModule(module)
             .then(() => {
-                Alert.alert('Succes', 'Module succesvol toegevoegd!');
+                alert('Module succesvol toegevoegd!');
                 setModule({
                     module_name: '',
                     description: '',
@@ -48,91 +49,39 @@ function AddModule() {
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.title}>Voeg een nieuwe module toe</Text>
-                <Button title="Terug naar module-platform" onPress={() => navigation.navigate('PlatformModules')} />
-            </View>
-            <View style={styles.form}>
-                <View style={styles.inputGroup}>
-                    <Text style={styles.label}>Module naam</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={module.module_name}
-                        onChangeText={(value) => handleChange('module_name', value)}
-                        required
-                    />
-                </View>
-                <View style={styles.inputGroup}>
-                    <Text style={styles.label}>Domein beschrijving</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={module.description}
-                        onChangeText={(value) => handleChange('description', value)}
-                        required
-                    />
-                </View>
-                <View style={styles.inputGroup}>
-                    <Text style={styles.label}>Progressie indicator</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={module.progress_indicator}
-                        onChangeText={(value) => handleChange('progress_indicator', value)}
-                        required
-                    />
-                </View>
-                <View style={styles.inputGroup}>
-                    <Text style={styles.label}>Domein</Text>
-                    <Picker
-                        selectedValue={module.domain_id}
-                        style={styles.input}
-                        onValueChange={(value) => handleChange('domain_id', value)}
-                        required
-                    >
-                        {modules.map((domain) => (
-                            <Picker.Item key={domain.id} label={domain.name} value={domain.id} />
-                        ))}
-                    </Picker>
-                </View>
-                <Button title="Voeg toe" onPress={handleSubmit} />
-            </View>
-        </View>
+        <BaseLayout>
+            <div className="container">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h1>Voeg een nieuwe module toe</h1>
+                    <Link to="/platform/modules" className="btn btn-secondary">Terug naar module-platform</Link>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="course_name" className="form-label">Module naam</label>
+                        <input type="text" className="form-control" id="course_name" name="course_name"
+                               value={module.module_name} onChange={handleChange} required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Domein beschrijving</label>
+                        <textarea className="form-control" id="description" name="description"
+                                  value={module.description} onChange={handleChange} required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="progression_indicator" className="form-label">Progressie indicator</label>
+                        <input type="text" className="form-control" id="progression_indicator" name="progression_indicator"
+                               value={module.module_name} onChange={handleChange} required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="domain_name" className="form-label">Domein:</label>
+                        <select id="domain_name" name="domain_name" onChange={handleChange} required>
+                            <option value={module.domain_id}>{module.domain_name}</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Voeg toe</button>
+                </form>
+            </div>
+        </BaseLayout>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-        backgroundColor: '#fff',
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 16,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-    form: {
-        flex: 1,
-    },
-    inputGroup: {
-        marginBottom: 16,
-    },
-    label: {
-        fontSize: 16,
-        marginBottom: 8,
-    },
-    input: {
-        height: 40,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        padding: 8,
-        borderRadius: 4,
-    },
-});
 
 export default AddModule;
