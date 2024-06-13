a50e5665020d65688c7e6ba26e979c167ff28cd7
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-06-13 20:36:26 +0200

    foutmeldingen opgelost

--- server/app.py
+++ server/app.py
@@ -2,19 +2,25 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from flask_bcrypt import Bcrypt
 import datetime
+import socket
 from database import (get_all_categories_from_database, enroll_student_in_database, get_student_domain,
                       get_course_name, delete_domain_from_database, edit_domain_in_database,
                       get_domain_from_database, add_domain_in_database, get_modules_from_database_by_domain_id,
-                      get_levels_by_module_id, get_assignment_by_assignment_id, add_user_to_db, get_user_from_db,
-                      add_module_in_database)
+                      get_module_by_id, edit_module_in_database,
+                      get_level_by_module_id, get_level_by_id, edit_level_in_database,
+                      add_user_to_db, get_user_from_db, delete_level_from_database, delete_module_from_database,
+                      get_all_levels_from_database, submit_teacher_review_in_database,
+                      get_submissions_for_level_from_database)
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 bcrypt = Bcrypt(app)
 
+
 @app.context_processor
 def inject_current_year():
     return {'current_year': datetime.datetime.now().year}
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -37,6 +43,7 @@ def register():
 
     return jsonify({"message": "Registratie succesvol"}), 201
 
+
 @app.route('/login', methods=['POST'])
 def login():
     email = request.json.get('email')
@@ -52,31 +59,106 @@ def login():
 
     if not bcrypt.check_password_hash(user[2], password):
         return jsonify({"error": "Ongeldig wachtwoord"}), 401
+
+    # Return the studentnumber as part of the response
     return jsonify({"message": "Inloggen succesvol", "studentnumber": user[5]}), 200
+
 
 @app.route('/api/modules/<int:domain_id>', methods=['GET'])
 def get_modules(domain_id):
     modules = get_modules_from_database_by_domain_id(domain_id)
     return jsonify(modules)
 
+
+@app.route('/api/modules/<int:module_id>', methods=['GET'])
+def get_modoule_by_id(module_id):
+    module = get_module_by_id(module_id)
+    return jsonify(module)
+
+
+@app.route('/api/change-module/<int:module_id>', methods=['PUT', 'GET'])
+def edit_module(module_id):
+    data = request.get_json()
+    try:
+        edit_module_in_database(module_id, data)
+        return jsonify({'message': 'Module succesvol gewijzigd'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Er is een fout opgetreden bij het wijzigen van het Module: ' + str(e)}), 400
+
+
+@app.route('/api/modules/<int:course_id>', methods=['DELETE'])
+def delete_module(module_id):
+    try:
+        delete_module_from_database(module)
+        return {'message': 'Moddule succesvol verwijderd'}, 200
+    except Exception as e:
+        return {'message': 'Er is een fout opgetreden bij het verwijderen van module: ' + str(e)}, 400
+
+
 @app.route('/api/levels/<int:module_id>', methods=['GET'])
 def get_level(module_id):
-    levels = get_levels_by_module_id(module_id)
-    print(levels)
+    levels = get_level_by_module_id(module_id)
     return jsonify(levels)
 
-@app.route('/api/assignment/<int:assignment_id>', methods=['GET'])
-def get_assignment(assignment_id):
-    assignment = get_assignment_by_assignment_id(assignment_id)
-    if assignment:
-        return jsonify(assignment), 200
-    else:
-        return jsonify({'message': 'Assignment niet gevonden'}), 404
+
+@app.route('/api/levels', methods=['GET'])
+def get_all_levels():
+    levels = get_all_levels_from_database()
+    return jsonify(levels)
+
+
+@app.route('/api/levels/<int:module_id>', methods=['GET'])
+def get_level_by_id(assignment_id):
+    level = get_level_by_id(assignment_id)
+    return jsonify(level)
+
+
+@app.route('/api/change-level/<int:course_id>', methods=['PUT', 'GET'])
+def edit_level(assignment_id):
+    data = request.get_json()
+    try:
+        edit_level_in_database(assignment_id, data)
+        return jsonify({'message': 'Level succesvol gewijzigd'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Er is een fout opgetreden bij het wijzigen van het Level: ' + str(e)}), 400
+
+
+@app.route('/api/levels/<int:assignment_idd>', methods=['DELETE'])
+def delete_level(assignment_id):
+    try:
+        delete_level_from_database(assignment_id)
+        return {'message': 'Level succesvol verwijderd'}, 200
+    except Exception as e:
+        return {'message': 'Er is een fout opgetreden bij het verwijderen van level: ' + str(e)}, 400
+
+
+@app.route('/api/submissions/<int:assignment_id>', methods=['GET'])
+def get_submissions_for_level(assignment_id):
+    submissions = get_submissions_for_level_from_database(assignment_id)
+    return jsonify(submissions)
+
+
+@app.route('/api/review/<int:submission_id>', methods=['PUT'])
+def submit_teacher_review(submission_id):
+    data = request.get_json()
+    teacher_review = data.get('teacher_review')
+    rating = data.get('rating')
+
+    if not teacher_review or not rating:
+        return jsonify({'error': 'Incomplete data provided'}), 400
+
+    try:
+        submit_teacher_review_in_database(submission_id, teacher_review, rating)
+        return jsonify({'message': 'Review submitted successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': f'Failed to submit review: {str(e)}'}), 500
+
 
 @app.route('/api/domains', methods=['GET'])
 def get_data():
     data = get_all_categories_from_database()
     return jsonify(data)
+
 
 @app.route('/api/enroll', methods=['POST'])
 def enroll_student():
@@ -91,6 +173,7 @@ def enroll_student():
 
     return {'message': 'Student enrolled successfully'}, 200
 
+
 @app.route('/api/check_enrollment', methods=['GET'])
 def check_enrollment():
     studentnumber = request.args.get('studentnumber')
@@ -103,6 +186,7 @@ def check_enrollment():
 
     return {'course_name': course_name, 'course_id': course_id}, 200
 
+
 @app.route('/api/domains/<int:course_id>', methods=['DELETE'])
 def delete_domain(course_id):
     try:
@@ -111,6 +195,7 @@ def delete_domain(course_id):
     except Exception as e:
         return {'message': 'Er is een fout opgetreden bij het verwijderen van het domein: ' + str(e)}, 400
 
+
 @app.route('/api/get-domain/<int:course_id>', methods=['GET'])
 def get_domain(course_id):
     domain = get_domain_from_database(course_id)
@@ -118,6 +203,7 @@ def get_domain(course_id):
         return jsonify(domain), 200
     else:
         return jsonify({'message': 'Domein niet gevonden'}), 404
+
 
 @app.route('/api/change-domain/<int:course_id>', methods=['PUT', 'GET'])
 def edit_domain(course_id):
@@ -128,27 +214,22 @@ def edit_domain(course_id):
     except Exception as e:
         return jsonify({'message': 'Er is een fout opgetreden bij het wijzigen van het domein: ' + str(e)}), 400
 
-@app.route('/api/add-domain/', methods=['POST', 'OPTIONS', 'GET'])
+
+@app.route('/api/add-domain/', methods=['POST', 'OPTIONS'])
 @cross_origin(supports_credentials=True)
 def create_domain():
     if request.method == 'OPTIONS':
+        # Preflight request. Reply successfully:
         return jsonify({'message': 'success'}), 200
     else:
+        # Actual request; handle POST.
         data = request.get_json()
-        print('Ontvangen domeingegevens:', data)
         result = add_domain_in_database(data)
         return result
 
-@app.route('/api/add-module/', methods=['POST'])
-def create_module():
-    data = request.get_json()
-    print('Ontvangen modulegegevens:', data)
-
-    if 'course_id' in data:
-        data['domain_id'] = data.pop('course_id')
-
-    result = add_module_in_database(data)
-    return result
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='192.168.1.225', port=5000)
+    # app.run(host='192.168.1.127', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
