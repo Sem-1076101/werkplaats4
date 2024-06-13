a4902d0ac66d1fffe9ca0817e0b22cbb3246fd69
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-06-13 20:36:33 +0200

    server ip veranderd

--- glitch/config.js
+++ glitch/config.js
@@ -1,4 +1,6 @@
 // export const SERVER_IP = '192.168.1.127';
 
 // export const SERVER_IP = '145.137.121.189';
-export const SERVER_IP = '192.168.1.127';
+// export const SERVER_IP = '192.168.1.127'
+export const SERVER_IP = '192.168.1.225';
+
