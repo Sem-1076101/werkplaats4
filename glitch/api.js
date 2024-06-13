41a3ef1c501ed1d70a48825f4b38c7dca4ad56bd
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-06-13 20:36:09 +0200

    get module by id toegevoegd gin g niet mee in commits

--- glitch/api.js
+++ glitch/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import {SERVER_IP} from './config';
+import { SERVER_IP } from './config';
 
 const connection = axios.create({
     baseURL: `http://${SERVER_IP}:5000`,
+
 });
 
 export function enrollStudent(studentId, courseId) {
@@ -10,6 +11,22 @@ export function enrollStudent(studentId, courseId) {
         student_id: studentId,
         course_id: courseId
     })
+}
+
+export function get_module_by_id(module_id) {
+    return connection.get(`/api/modules/${module_id}`)
+        .then(response => {
+            if (response) {
+                return response.data;
+            } else {
+                throw new Error('Response is undefined');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching levels', error);
+            console.error('Error fetching levels details ', error.response.data);
+            throw error;
+        });
 }
 
 export function get_modules(domain_id) {
@@ -29,7 +46,7 @@ export function get_modules(domain_id) {
         });
 }
 
-export function get_levels_from_module(module_id) {
+export function get_level(module_id) {
     return connection.get(`/api/levels/${module_id}`)
         .then(response => {
             if (response) {
@@ -45,8 +62,8 @@ export function get_levels_from_module(module_id) {
         });
 }
 
-export function get_assignment_by_assignment_id(assignment_id) {
-    return connection.get(`/api/assignment/${assignment_id}`)
+export function get_all_levels() {
+        return connection.get(`/api/levels`)
         .then(response => {
             if (response) {
                 return response.data;
@@ -55,22 +72,106 @@ export function get_assignment_by_assignment_id(assignment_id) {
             }
         })
         .catch(error => {
-            console.error('Fout bij het ophalen van het assignment:', error);
+            console.error('Error fetching levels', error);
+            console.error('Error fetching levels details ', error.response.data);
             throw error;
         });
 }
 
+export function deleteLevel(assignment_id) {
+    return connection.delete(`/api/domains/${assignment_id}`)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+export function editLevel(assignment_id, level) {
+    return connection.put(`/api/change-level/${assignment_id}`, level)
+        .then(response => {
+            if (response) {
+                return response.data;
+            } else {
+                throw new Error('Response is undefined');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            throw error; // re-throw the error so it can be caught in EditDomain.jsx
+        });
+}
+
+export function get_level_by_id(assignment_id) {
+    return connection.get(`/api/levels/${assignment_id}`)
+        .then(response => {
+            if (response) {
+                return response.data;
+            } else {
+                throw new Error('Response is undefined');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching levels', error);
+            console.error('Error fetching levels details ', error.response.data);
+            throw error;
+        });
+}
+
+
+export function getSubmissionsForLevel(assignment_id) {
+    return connection.get(`/api/submissions/${assignment_id}`)
+        .then(response => {
+            if (response && response.data) {
+                return response.data;
+            } else {
+                console.error('Fout bij ophalen van ingeleverde opdrachten voor level:', assignment_id);
+                throw new Error('Geen data-eigenschap in de reactie');
+            }
+        })
+        .catch(error => {
+            console.error('Fout bij ophalen van ingeleverde opdrachten:', error);
+            throw error;
+        });
+}
+
+export function submitTeacherReview(submission_id, teacherReview, rating) {
+    return connection.put(`/api/review/${submission_id}`, {
+        teacher_review: teacherReview,
+        rating: rating
+    })
+        .then(response => {
+            if (response && response.data) {
+                return response.data;
+            } else {
+                console.error('Fout bij opslaan van beoordeling voor inzending:', submission_id);
+                throw new Error('Geen data-eigenschap in de reactie');
+            }
+        })
+        .catch(error => {
+            console.error('Fout bij opslaan van beoordeling:', error);
+            throw error;
+        });
+}
+
+
+
 export function checkEnrollment(studentId) {
     return connection.get(`/api/check_enrollment?studentnumber=${studentId}`)
         .then(response => {
-                if (response.data.course_name && response.data.course_id) {
-                    return {
-                        course_name: response.data.course_name,
-                        course_id: response.data.course_id
-                    };
-                }
+            if (response.data.course_name && response.data.course_id) {
+                return {
+                    course_name: response.data.course_name,
+                    course_id: response.data.course_id
+                };
+            } else {
+                throw new Error('No course name or course id returned from server');
             }
-        )
+        })
+        .catch(error => {
+            console.error('Error checking enrollment:', error);
+        });
 }
 
 export function domains() {
@@ -124,35 +225,22 @@ export function getDomain(courseId) {
 }
 
 export function addDomain(domain) {
-    return axios.post(`http://${SERVER_IP}:5000/api/add-domain/`, domain, {
+    return connection.post('/api/add-domain/', domain, {
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => {
-        if (response) {
-            return response.data;
-        } else {
-            throw new Error('Response is undefined');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        throw error;
-    });
-}
-
-export function addModule(moduleData) {
-    return connection.post('/api/add-module/', moduleData)
         .then(response => {
-            return response.data;
+            if (response) {
+                return response.data;
+            } else {
+                throw new Error('Response is undefined');
+            }
         })
         .catch(error => {
-            console.error('Error adding module:', error);
+            console.error('Error:', error);
             throw error;
         });
 }
-
-
 
 export default connection;
