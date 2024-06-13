5adb502a9611ac24d60e28eb98d8ad0c1fec9d24
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-06-13 20:36:52 +0200

    Database aanpassingen

--- server/database.py
+++ server/database.py
@@ -86,16 +86,27 @@ def add_domain_in_database(domain):
     conn = get_db()
     cursor = conn.cursor()
     try:
-        cursor.execute("INSERT INTO domains (course_name, course_description) VALUES (?, ?)",
-                       (domain['course_name'], domain['course_description']))
+        course_image = base64.b64decode(domain['course_image']) if domain.get('course_image') else None
+        cursor.execute("INSERT INTO domains (course_name, course_description, course_image) VALUES (?, ?, ?)",
+                       (domain['course_name'], domain['course_description'], course_image))
         conn.commit()
         return jsonify({'id': cursor.lastrowid})
-    except Exception as e:
-        print('Fout bij het toevoegen van het domein:', e)
-        return jsonify({'error': 'Er is een fout opgetreden tijdens het toevoegen van het domein.'}), 500
     finally:
         conn.close()
 
+
+def get_module_by_id(module_id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM modules WHERE module_id=?", (module_id,))
+    columns = [column[0] for column in cursor.description]
+    levels = cursor.fetchall()
+    conn.close()
+    data = []
+    for row in levels:
+        row_dict = dict(zip(columns, row))
+        data.append(row_dict)
+    return data
 
 
 def get_modules_from_database_by_domain_id(domain_id):
@@ -112,7 +123,26 @@ def get_modules_from_database_by_domain_id(domain_id):
     return data
 
 
-def get_levels_by_module_id(module_id):
+def edit_module_in_database(module_id, module):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE modules SET module_name=?, description=?, progress_indicator=?, domain_id=? WHERE module_id=?",
+        (module['module_name'], module['description'], module['progress_indicator'], module['domain_id'], module_id)
+    )
+    conn.commit()
+    conn.close()
+
+
+def delete_module_from_database(module_id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM modules WHERE id=?", (module_id,))
+    conn.commit()
+    conn.close()
+
+
+def get_level_by_module_id(module_id):
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM levels WHERE module_id=?", (module_id,))
@@ -126,26 +156,89 @@ def get_levels_by_module_id(module_id):
     return data
 
 
-def get_assignment_by_assignment_id(assignment_id):
-    conn = get_db()  # Je moet nog de get_db() functie implementeren om de databaseverbinding te krijgen
+def get_level_by_id(assignment_id):
+    conn = get_db()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM levels WHERE assignment_id=?", (assignment_id,))
     columns = [column[0] for column in cursor.description]
-    assignment = cursor.fetchone()
+    levels = cursor.fetchall()
     conn.close()
-    if assignment:
-        assignment_dict = dict(zip(columns, assignment))
-        return assignment_dict
-    else:
-        return None
+    data = []
+    for row in levels:
+        row_dict = dict(zip(columns, row))
+        data.append(row_dict)
+    return data
+
+
+def get_all_levels_from_database():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM levels")
+    columns = [column[0] for column in cursor.description]
+    levels = cursor.fetchall()
+    conn.close()
+    data = []
+    for row in levels:
+        row_dict = dict(zip(columns, row))
+        data.append(row_dict)
+    return data
+
+
+
+
+def edit_level_in_database(assignment_id, level):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE levels SET assignment_title=?, assignment_description=? WHERE assignment_id=?",
+        (level['assignment_title'], level['assignment_description'], assignment_id)
+    )
+    conn.commit()
+    conn.close()
+
+
+def submit_teacher_review_in_database(submission_id, teacher_review, rating):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE submissions SET teacher_review=?, rating=? WHERE id=?",
+        (teacher_review, rating, submission_id)
+    )
+    conn.commit()
+    conn.close()
+
+
+def get_submissions_for_level_from_database(assignment_id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM submissions WHERE assignment_id=?", (assignment_id,))
+    columns = [column[0] for column in cursor.description]
+    submissions = cursor.fetchall()
+    conn.close()
+    data = []
+    for row in submissions:
+        row_dict = dict(zip(columns, row))
+        data.append(row_dict)
+    return data
+
+
+def delete_level_from_database(assignment_id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM levels WHERE assignment_id=?", (assignment_id,))
+    conn.commit()
+    conn.close()
 
 
 def add_user_to_db(table, email, password, first_name, last_name, studentnumber):
     conn = sqlite3.connect('instance/glitch.db')
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO {table} (email, password, first_name, last_name, studentnumber) VALUES (?, ?, ?, ?, ?)", (email, password, first_name, last_name, studentnumber))
+    cursor.execute(
+        f"INSERT INTO {table} (email, password, first_name, last_name, studentnumber) VALUES (?, ?, ?, ?, ?)",
+        (email, password, first_name, last_name, studentnumber))
     conn.commit()
     conn.close()
+
 
 def get_user_from_db(email):
     conn = sqlite3.connect('instance/glitch.db')
@@ -154,18 +247,3 @@ def get_user_from_db(email):
     user = cursor.fetchone()
     conn.close()
     return user
-
-
-def add_module_in_database(data):
-    conn = get_db()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("INSERT INTO modules (module_name, description, domain_id) VALUES (?, ?, ?)",
-                       (data['module_name'], data['module_description'], data['domain_id']))
-        conn.commit()
-        return jsonify({'id': cursor.lastrowid}), 201
-    except Exception as e:
-        print('Fout bij het toevoegen van de module:', e)
-        return jsonify({'error': 'Er is een fout opgetreden tijdens het toevoegen van de module.'}), 500
-    finally:
-        conn.close()
