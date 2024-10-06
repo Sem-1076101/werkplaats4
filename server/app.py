eaaf227c51df6a9617f818b7776901c83bb2766d
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-10-06 22:24:19 +0200

    Salt hashing toegevoegd.

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 import datetime
 from database import (get_all_categories_from_database, enroll_student_in_database, get_student_domain,
@@ -16,11 +16,9 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 bcrypt = Bcrypt(app)
 
-
 @app.context_processor
 def inject_current_year():
     return {'current_year': datetime.datetime.now().year}
-
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -37,12 +35,13 @@ def register():
     if not email.endswith('@hr.nl'):
         return jsonify({"error": "Registratie is alleen toegestaan voor hr.nl e-mailadressen"}), 400
 
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
 
     add_user_to_db('students', email, hashed_password, first_name, last_name, studentnumber)
 
     return jsonify({"message": "Registratie succesvol"}), 201
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -63,18 +62,15 @@ def login():
     # Return the studentnumber and course_id as part of the response
     return jsonify({"message": "Inloggen succesvol", "studentnumber": user[5], "course_id": user[7]}), 200
 
-
 @app.route('/api/modules/<int:domain_id>', methods=['GET'])
 def get_modules(domain_id):
     modules = get_modules_from_database_by_domain_id(domain_id)
     return jsonify(modules)
 
-
 @app.route('/api/modules/<int:module_id>', methods=['GET'])
-def get_modoule_by_id(module_id):
+def get_module_by_id(module_id):
     module = get_module_by_id(module_id)
     return jsonify(module)
-
 
 @app.route('/api/change-module/<int:module_id>', methods=['PUT', 'GET'])
 def edit_module(module_id):
@@ -85,33 +81,28 @@ def edit_module(module_id):
     except Exception as e:
         return jsonify({'message': 'Er is een fout opgetreden bij het wijzigen van het Module: ' + str(e)}), 400
 
-
-@app.route('/api/modules/<int:course_id>', methods=['DELETE'])
+@app.route('/api/modules/<int:module_id>', methods=['DELETE'])
 def delete_module(module_id):
     try:
         delete_module_from_database(module_id)
-        return {'message': 'Moddule succesvol verwijderd'}, 200
+        return {'message': 'Module succesvol verwijderd'}, 200
     except Exception as e:
         return {'message': 'Er is een fout opgetreden bij het verwijderen van module: ' + str(e)}, 400
-
 
 @app.route('/api/levels/<int:module_id>', methods=['GET'])
 def get_level(module_id):
     levels = get_level_by_module_id(module_id)
     return jsonify(levels)
 
-
 @app.route('/api/levels', methods=['GET'])
 def get_all_levels():
     levels = get_all_levels_from_database()
     return jsonify(levels)
 
-
-@app.route('/api/levels/<int:module_id>', methods=['GET'])
+@app.route('/api/levels/<int:assignment_id>', methods=['GET'])
 def get_level_by_id(assignment_id):
     level = get_level_by_id(assignment_id)
     return jsonify(level)
-
 
 @app.route('/api/change-level/<int:course_id>', methods=['PUT', 'GET'])
 def edit_level(assignment_id):
@@ -122,7 +113,6 @@ def edit_level(assignment_id):
     except Exception as e:
         return jsonify({'message': 'Er is een fout opgetreden bij het wijzigen van het Level: ' + str(e)}), 400
 
-
 @app.route('/api/levels/<int:assignment_id>', methods=['DELETE'])
 def delete_level(assignment_id):
     try:
@@ -131,12 +121,10 @@ def delete_level(assignment_id):
     except Exception as e:
         return {'message': 'Er is een fout opgetreden bij het verwijderen van level: ' + str(e)}, 400
 
-
 @app.route('/api/submissions/<int:assignment_id>', methods=['GET'])
 def get_submissions_for_level(assignment_id):
     submissions = get_submissions_for_level_from_database(assignment_id)
     return jsonify(submissions)
-
 
 @app.route('/api/review/<int:submission_id>', methods=['PUT'])
 def submit_teacher_review(submission_id):
@@ -153,12 +141,10 @@ def submit_teacher_review(submission_id):
     except Exception as e:
         return jsonify({'error': f'Failed to submit review: {str(e)}'}), 500
 
-
 @app.route('/api/domains', methods=['GET'])
 def get_data():
     data = get_all_categories_from_database()
     return jsonify(data)
-
 
 @app.route('/api/enroll', methods=['POST'])
 def enroll_student():
@@ -173,7 +159,6 @@ def enroll_student():
 
     return {'message': 'Student enrolled successfully'}, 200
 
-
 @app.route('/api/check_enrollment', methods=['GET'])
 def check_enrollment():
     studentnumber = request.args.get('studentnumber')
@@ -186,7 +171,6 @@ def check_enrollment():
 
     return {'course_name': course_name, 'course_id': course_id}, 200
 
-
 @app.route('/api/domains/<int:course_id>', methods=['DELETE'])
 def delete_domain(course_id):
     try:
@@ -195,7 +179,6 @@ def delete_domain(course_id):
     except Exception as e:
         return {'message': 'Er is een fout opgetreden bij het verwijderen van het domein: ' + str(e)}, 400
 
-
 @app.route('/api/get-domain/<int:course_id>', methods=['GET'])
 def get_domain(course_id):
     domain = get_domain_from_database(course_id)
@@ -203,7 +186,6 @@ def get_domain(course_id):
         return jsonify(domain), 200
     else:
         return jsonify({'message': 'Domein niet gevonden'}), 404
-
 
 @app.route('/api/change-domain/<int:course_id>', methods=['PUT', 'GET'])
 def edit_domain(course_id):
@@ -214,7 +196,6 @@ def edit_domain(course_id):
     except Exception as e:
         return jsonify({'message': 'Er is een fout opgetreden bij het wijzigen van het domein: ' + str(e)}), 400
 
-
 @app.route('/api/add-domain/', methods=['POST', 'OPTIONS'])
 def create_domain():
     if request.method == 'OPTIONS':
@@ -223,7 +204,6 @@ def create_domain():
         data = request.get_json()
         result = add_domain_in_database(data)
         return result
-
 
 @app.route('/api/add-level/', methods=['POST', 'OPTIONS'])
 def create_level():
@@ -234,7 +214,6 @@ def create_level():
         result = add_level_in_database(data)
         return jsonify(result)
 
-
 @app.route('/api/add-module/', methods=['POST', 'OPTIONS'])
 def create_module():
     if request.method == 'OPTIONS':
@@ -244,24 +223,11 @@ def create_module():
         result = add_module_in_database(data)
         return jsonify(result)
 
-
 @app.route('/api/modules/', methods=['GET'])
 def get_module():
     module = get_module_from_database()
     return jsonify(module)
 
-
 @app.route('/api/update_point_challenge/<int:studentnumber>', methods=['POST'])
 def update_challenge(studentnumber):
     update_point_challenge(studentnumber)
-    return jsonify({'message': 'Point challenge updated successfully'}), 200
-
-
-@app.route('/api/student/<int:studentnumber>', methods=['GET'])
-def get_studentnumber(studentnumber):
-    student = get_student_by_studentnumber(studentnumber)
-    return jsonify(student)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
